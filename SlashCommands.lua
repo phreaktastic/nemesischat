94880239cf857ff6101524dfc4b32699f668b0a8
phreaktastic <137018157+phreaktastic@users.noreply.github.com>
2023-06-18 22:11:23 -0600

    Remove testing commands

--- SlashCommands.lua
+++ SlashCommands.lua
@@ -14,36 +14,6 @@ local _, core = ...;
 function NemesisChat:SlashCommand(msg)
 	if not msg or msg:trim() == "" then
 		InterfaceOptionsFrame_OpenToCategory(core.optionsFrame)
-    elseif msg == "nemeses" then
-        self:Print("Total nemeses defined:", NemesisChat:GetNemesesLength())
-    elseif msg == "nemeses list" then 
-        self:Print("Nemeses list:")
-        for key,val in pairs(NemesisChat:GetNemeses()) do
-            self:Print(key, val)
-        end
-    elseif msg == "phrases" then
-        self:Print("Total phrases defined:", NemesisChat:GetMessagesLength())
-    elseif msg == "phrases interrupt" then
-        self:Print("Total interrupt phrases defined:", NemesisChat:GetLength(NemesisChat:GetAllInterruptPhrases()))
-    elseif msg == "party" then
-        NemesisChat:CheckGroup()
-    elseif msg == "partynemeses" then
-        local nemeses = NemesisChat:GetPartyNemeses()
-
-        if nemeses == nil then
-            self:Print("NONE")
-            return
-        end
-
-        for key,val in pairs(nemeses) do
-            self:Print(key)
-        end
-    elseif msg == "status" then
-        self:Print("NC Status", NCStatus)
-        core.runtime.event.category = "BOSS"
-        core.runtime.event.event = "GREATERDPS"
-        core.runtime.event.target = "SELF"
-        self:Print(NemesisChat:GetRandomAiPhrase())
 	else
         if core.db.profile.dbg then
             self:Print("Invalid command issued.")
