5e3f9797b0fef8f2b7577eee2b8313eaa47d2a84
phreaktastic <137018157+phreaktastic@users.noreply.github.com>
2024-01-19 15:25:31 -0700

    1.9.10

--- Models/Controller.lua
+++ Models/Controller.lua
@@ -536,8 +536,14 @@ function NemesisChat:InstantiateController()
         ["HAS_BUFF"] = function(val1, val2)
             return NemesisChat:HasBuff(val1, val2)
         end,
+        ["NOT_HAS_BUFF"] = function(val1, val2)
+            return not NemesisChat:HasBuff(val1, val2)
+        end,
         ["HAS_DEBUFF"] = function(val1, val2)
             return NemesisChat:HasDebuff(val1, val2)
+        end,
+        ["NOT_HAS_DEBUFF"] = function(val1, val2)
+            return not NemesisChat:HasDebuff(val1, val2)
         end,
     }
 end
